5cd4d9053cc3cf420e7a03e7c8238ff3233c2c24
jamgar <jegpub@gmail.com>
2017-03-20 21:36:56 -0500

    Add registration

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,4 +1,23 @@
 class V1::UsersController < ApplicationController
-  def new
+  
+  def show
+    user = User.find(params[:id])
+    render json: user, status: :ok
   end
+    
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: {status: 'User created successfully'}, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
+    post '/signup', to: 'users#create'
     resources :contacts
-    get 'users/new'
+    resources :users
   end
 end
